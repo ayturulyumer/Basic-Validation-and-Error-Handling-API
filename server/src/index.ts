e2fd4c31dcae88f073213c99b3cb73ea29ec575c
ajummer <makeudreamempty@gmail.com>
2025-06-02 21:59:58 +0300

    feat: pass error handler to app

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,15 @@
 import express, { Request, Response } from "express";
 import routes from "./routes";
+import errorHandler from "./middlewares/errorHandler";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+
 app.use("/", routes);
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
